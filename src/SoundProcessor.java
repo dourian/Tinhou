bb111c5198337a7f4c975f47c26559945ee49230
GTinCan <49460053+GTinCan@users.noreply.github.com>
2022-01-06 21:49:11 -0500

    small changes to Main and added SoundProcessor class

---
+++ src/SoundProcessor.java
@@ -0,0 +1,11 @@
+
+/**
+ * @author max13
+ * sound processor class handles the input and output of sound files into audio and extracts characteristics of songs that can be interpreted as bullet patterns
+ */
+public class SoundProcessor {
+	private short[] data;
+	SoundProcessor(String fileName) {
+		
+	}
+}
