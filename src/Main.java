f4e4ff2a8031a0c1603f1705417bd723e71cb017
dourian <75771131+dourian@users.noreply.github.com>
2022-01-13 09:40:18 -0500

    added 2 screens and worked on navigation

--- src/Main.java
+++ src/Main.java
@@ -1,38 +1,61 @@
 import java.awt.*;
 import java.awt.event.*;
+import java.util.Stack;
 import javax.swing.*;
 
-
-public class Main extends JPanel implements Runnable, MouseListener {
+public class Main extends JPanel implements Runnable, MouseListener, KeyListener {
 	Game game;
-	
+
 	static JPanel panel;
 	static JFrame frame;
 	Image [] images;
-	
-	int currentScreen = 0;
-	
+	Image offScreenImage;
+	Graphics offScreenBuffer;
+	static Stack navigation;
+
+	final int HOME=0, PLAY = 1, LEADERBOARD = 2, SETTINGS = 3;
+	static int gameState = 0;
+
 	Main() {
 		game = new Game(700, 1000, false, "keshi 2.wav");
 		addKeyListener((KeyListener) game.getListener());
 		
-		images = new Image [1];
-		images[0] = Toolkit.getDefaultToolkit().getImage("openingscreen.png");
-		
+		navigation = new Stack <Integer> ();
+		navigation.add(HOME);
+
+		images = new Image [10];
+		images[HOME] = Toolkit.getDefaultToolkit().getImage("openingscreen.png");
+		images[LEADERBOARD] = Toolkit.getDefaultToolkit().getImage("leaderboardscreen.png");
+		images[SETTINGS] = Toolkit.getDefaultToolkit().getImage("settingsscreen.png");
+
+
 		addMouseListener (this);
-		
+		frame.addKeyListener (this);
 	}
-	
+
 	public void clearBoard(Graphics g) {
+		g.setColor(Color.WHITE);
 		g.fillRect(0, 0, 1000, 700);
 	}
-	
+
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
-		if (currentScreen==0)g.drawImage(images[0],0, 0, 1000, 700,this);
-		else if (currentScreen==1)game.repaint(g);
+
+		if (offScreenBuffer == null)
+		{
+			offScreenImage = createImage (1000,  700);
+			offScreenBuffer = offScreenImage.getGraphics ();
+		}
+
+		if (gameState!=PLAY) {
+//			System.out.println(gameState);
+			update(g, gameState);
+		}
+		else {
+			game.repaint(g);
+		}
 	}
-	
+
 	public static void main(String args[]) {
 		frame = new JFrame ();
 		frame.setPreferredSize(new Dimension(1000, 700));
@@ -41,30 +64,42 @@ public class Main extends JPanel implements Runnable, MouseListener {
 		frame.pack ();
 		frame.setVisible (true);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
+
 	}
-	
+
+	public void update(Graphics g, int state) {
+		if (offScreenBuffer == null)
+		{
+			offScreenImage = createImage (1000,  700);
+			offScreenBuffer = offScreenImage.getGraphics ();
+		}
+//		System.out.println(state);
+		offScreenBuffer.clearRect (0, 0, 1920, 1080);
+		offScreenBuffer.drawImage(images[state],0, 0, 1000, 700,this);
+		g.drawImage(offScreenImage,0,0,this);
+	}
+
 	public int buttonTracker(int x, int y) {
 		if (x>300 && x<700) {
 			if (y>325 && y<425) {
-				return 1;
+				return PLAY;
 			}
 			else if (y>450 && y<525) {
-				return 2;
+				return LEADERBOARD;
 			}
 			else if (y>550 && y<625) {
-				return 3;
+				return SETTINGS;
 			}
 		}
-		
+
 		return -1;
 	}
-	
+
 	@Override
 	public void run() {
 		playGame();
 	}
-	
+
 	public void playGame() {
 		game.playAudio();
 		while(true) {
@@ -75,38 +110,68 @@ public class Main extends JPanel implements Runnable, MouseListener {
 	}
 	@Override
 	public void mouseClicked(MouseEvent e) {
-		/*
-		 * 1: PLAY
-		 * 2: LEADERBOARD
-		 * 3: SETTINGS
-		 */
-		
-		if (buttonTracker(e.getX(), e.getY())==1) {
-			clearBoard(getGraphics());
+
+		if (buttonTracker(e.getX(), e.getY())==PLAY) {
+			gameState = PLAY;
+			navigation.push(PLAY);
+			repaint();
 			new Thread((Runnable) panel).start();
-			game.repaint(getGraphics());
-			currentScreen=1;
 		}
-		
+		else if (buttonTracker(e.getX(), e.getY())==LEADERBOARD) {
+			gameState = LEADERBOARD;
+			navigation.push(LEADERBOARD);
+			repaint();
+		}
+		else if (buttonTracker(e.getX(), e.getY())==SETTINGS) {
+			gameState = SETTINGS;
+			navigation.push(SETTINGS);
+			repaint();
+		}
+
 	}
 	@Override
 	public void mousePressed(MouseEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 	@Override
 	public void mouseReleased(MouseEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 	@Override
 	public void mouseEntered(MouseEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 	@Override
 	public void mouseExited(MouseEvent e) {
 		// TODO Auto-generated method stub
-		
+
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		System.out.println("esc");
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		System.out.println("esc");
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		System.out.println("esc");
+		if (e.getKeyCode()==KeyEvent.VK_ESCAPE) {
+			if (navigation.size()> 1 && gameState!=PLAY) {
+				navigation.pop();
+				gameState = (int) navigation.peek();
+				repaint();
+			}
+		}
 	}
 }
