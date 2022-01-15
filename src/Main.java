d08b17262568e20ec64cf90985d00d0d680725bd
GTinCan <49460053+GTinCan@users.noreply.github.com>
2022-01-15 11:33:49 -0500

    added experimental central listener. probably not the best idea to use this

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 import java.awt.*;
 import java.awt.event.*;
+import java.util.BitSet;
 import java.util.Stack;
+import java.util.Vector;
+
 import javax.swing.*;
 
 public class Main extends JPanel implements Runnable, MouseListener, KeyListener {
@@ -134,43 +137,14 @@ public class Main extends JPanel implements Runnable, MouseListener, KeyListener
 		}
 
 	}
-	@Override
-	public void mousePressed(MouseEvent e) {
-		// TODO Auto-generated method stub
-
-	}
-	@Override
-	public void mouseReleased(MouseEvent e) {
-		// TODO Auto-generated method stub
-
-	}
-	@Override
-	public void mouseEntered(MouseEvent e) {
-		System.out.println(e.getX() +" "+ e.getY());
-
-	}
-	@Override
-	public void mouseExited(MouseEvent e) {
-		// TODO Auto-generated method stub
-
-	}
 	
-
-	public void mouseMoved(MouseEvent e) {
-		System.out.println(e.getX()+" "+e.getY());
-	}
-
-	@Override
-	public void keyTyped(KeyEvent e) {
-		// TODO Auto-generated method stub
-//		System.out.println("esc");
-	}
-
-	@Override
-	public void keyPressed(KeyEvent e) {
-		// TODO Auto-generated method stub
-//		System.out.println("esc");
-	}
+	public void mousePressed(MouseEvent e) { }
+	public void mouseReleased(MouseEvent e) { }
+	public void mouseEntered(MouseEvent e) { }
+	public void mouseExited(MouseEvent e) { }
+	public void mouseMoved(MouseEvent e) { }
+	public void keyTyped(KeyEvent e) { }
+	public void keyPressed(KeyEvent e) { }
 
 	@Override
 	public void keyReleased(KeyEvent e) {
@@ -183,5 +157,95 @@ public class Main extends JPanel implements Runnable, MouseListener, KeyListener
 				repaint();
 			}
 		}
+	}
+}
+
+
+class CentralListener implements MouseMotionListener, MouseListener, KeyListener, Runnable {
+
+	public final byte PRESS_EVENT = 1, RELEASE_EVENT = 0, NULL_EVENT = -1;
+	
+	private BitSet keyDown;			//true if key is held at given time
+	private BitSet keyPressed;		//true if key is held while previously key was unheld at given time
+	private BitSet mouseDown;		//true if mouse is held at given time
+	private BitSet mousePressed;	//true if mouse is held while previously mouse was unheld at given time
+	private int mouseX, mouseY;		//x and y position of cursor
+	
+	private JComponent jcomp;		//component to get info from
+	
+	Vector<EventClass> buffer;		//buffer (vectors are threadsafe)
+	
+	CentralListener(JComponent comp) {
+		
+		keyDown = new BitSet(1<<16);
+		keyPressed = new BitSet(1<<16);
+		mouseDown = new BitSet(MouseInfo.getNumberOfButtons()+1);
+		mousePressed = new BitSet(MouseInfo.getNumberOfButtons()+1);
+		
+		jcomp = comp;
+		jcomp.addMouseMotionListener(this);
+		jcomp.addMouseListener(this);
+		jcomp.addKeyListener(this);
+		
+		buffer = new Vector<EventClass>();
+		
+		new Thread(this).start();
+	}
+	
+	//set keys
+	public void keyTyped(KeyEvent e) { }
+	public void keyPressed(KeyEvent e) { buffer.add(new EventClass(e, PRESS_EVENT)); }
+	public void keyReleased(KeyEvent e) { buffer.add(new EventClass(e, PRESS_EVENT)); }
+	
+	//get keys
+	public boolean isHeld(int keyCode) { return keyDown.get(keyCode); }
+	public boolean isPressed(int keyCode) { return keyPressed.get(keyCode); }
+	
+	//set mouse
+	public void mouseClicked(MouseEvent e) { }
+	public void mousePressed(MouseEvent e) { buffer.add(new EventClass(e, PRESS_EVENT)); }
+	public void mouseReleased(MouseEvent e) { buffer.add(new EventClass(e, RELEASE_EVENT)); }
+	public void mouseEntered(MouseEvent e) { buffer.add(new EventClass(e, NULL_EVENT)); }
+	public void mouseExited(MouseEvent e) { buffer.add(new EventClass(e, NULL_EVENT)); }
+	public void mouseDragged(MouseEvent e) { buffer.add(new EventClass(e, NULL_EVENT)); }
+	public void mouseMoved(MouseEvent e) { buffer.add(new EventClass(e, NULL_EVENT)); }
+	
+	//get mouse info
+	public boolean mouseIsHeld(int button) {return mouseDown.get(button);}
+	public boolean mouseIsPressed(int button) {return mousePressed.get(button);}
+	public int getX() {return mouseX;}
+	public int getY() {return mouseY;}
+	
+	public void run() {
+		while(true) {
+			keyPressed.clear();
+			mousePressed.clear();
+			for(EventClass e: buffer) {
+				if(e.event instanceof KeyEvent) {
+					KeyEvent event = (KeyEvent)(e.event);
+					keyPressed.set(event.getKeyCode(), e.type==1?true:false);
+					keyDown.set(event.getKeyCode(), e.type==1?true:false);
+				}
+				if(e.event instanceof MouseEvent) {
+					MouseEvent event = (MouseEvent)(e.event);
+					if(e.type != NULL_EVENT) {
+						mousePressed.set(event.getButton(), e.type==1?true:false);
+						mouseDown.set(event.getButton(), e.type==1?true:false);
+					}
+					mouseX = event.getX();
+					mouseY = event.getY();
+				}
+			}
+			buffer.clear();
+			try {
+				Thread.sleep(20);
+			} catch (InterruptedException e1) { e1.printStackTrace(); }
+		}
+	}
+
+	private class EventClass {
+		InputEvent event;
+		byte type;
+		EventClass(InputEvent e, byte t) {event = e; type = t;}
 	}
 }
