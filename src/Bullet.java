a509626a3372a5bfab5f95338367ff922dd34df5
GTinCan <49460053+GTinCan@users.noreply.github.com>
2022-01-24 09:00:44 -0500

    bullet comments

--- src/Bullet.java
+++ src/Bullet.java
@@ -8,22 +8,28 @@ import javax.imageio.ImageIO;
  * bullet class. stores bullet data.
  */
 public class Bullet extends Entity {
-	static Image[] icons;
-	static double[] radii;
+	static Image[] icons;		//list of possible bullet icons
+	static double[] radii;		//list of possible bullet radii corresponding to icon
+	private double timetolive;	//amount of time that bullet can exist
 	static {
 		icons = new Image[6];
 		radii = new double[6];
 		for(int i = 0; i < 6; i++) {
 			try {
-				icons[i] = ImageIO.read(new File("bullet" + (i+1) + ".png"));
+				icons[i] = ImageIO.read(new File("bullet" + (i+1) + ".png")); //static constructor that initializes icons and radii
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
 			radii[i] = (double)icons[i].getWidth(null)/2;
 		}
 	}
-	
-	private double timetolive;
+	/**
+	 * constructor for bullet
+	 * @param position initial position of bullet
+	 * @param velocity initial velocity of bullet
+	 * @param type type of bullet,
+	 * must be in range [0,6)
+	 */
 	Bullet(Complex position, Complex velocity, int type) {
 		super(position, velocity, radii[type], icons[type]);
 		timetolive = 3;
@@ -34,12 +40,18 @@ public class Bullet extends Entity {
 		g.drawImage(icon, 0, 0, null);
 		icon = rotated;
 	}
-	
+	/**
+	 * overrides cycle function of entity.
+	 * @param f time elapsed since last cycle
+	 * @return if bullet may still exist
+	 */
 	public boolean cycle(float f) {
 		timetolive -= f;
 		pos = pos.plus(vel.mult(f));
 		return timetolive > 0;
 	}
-	
+	/**
+	 * @return angle of bullet velocity
+	 */
 	public double angle() { return vel.arg(); }
 }
